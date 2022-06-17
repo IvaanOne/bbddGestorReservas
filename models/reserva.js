9468b0554415a9fa0e50aa20052746bc8c6d9cf6
IvaanOne <ivaanone17@gmail.com>
2022-06-17 11:13:09 +0200

    Arreglando foreignkeys

--- models/reserva.js
+++ models/reserva.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'hotelName'
       });
       movie.belongsTo(models.cliente, {
-        foreignKey: 'clientName'
+        foreignKey: 'id'
       });
     }
   }
